8099dc73b7a7f278431851e09f3ea34be81ba50e
Travis Nazelrod <travisnazelrod07@gmail.com>
2018-06-20 20:03:02 -0700

    commit 3

---
+++ wine cellar/WineMapViewController.swift
@@ -0,0 +1,71 @@
+//
+//  WineMapViewController.swift
+//  wine cellar
+//
+//  Created by Travis Nazelrod on 6/20/18.
+//  Copyright © 2018 Travis Nazelrod. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class WineMapViewController: UIViewController {
+    
+    let locationManager = CLLocationManager()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //set up location service
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+/*
+ // MARK : - CLLocationManagerDelegate functions
+ */
+extension WineMapViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            locationManager.requestLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if locations.first != nil{
+            print("location:: (location)")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("error:: (error)")
+    }
+    
+}
+
+
+
+
