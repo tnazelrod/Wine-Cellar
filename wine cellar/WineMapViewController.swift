942ed0d05f2dfe66258f05621330bb45428b5cdf
Travis Nazelrod <travisnazelrod07@gmail.com>
2018-06-22 21:14:31 -0700

    6/22/18 edits

--- wine cellar/WineMapViewController.swift
+++ wine cellar/WineMapViewController.swift
@@ -13,7 +13,8 @@ class WineMapViewController: UIViewController {
     
     let locationManager = CLLocationManager()
     
-
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -55,8 +56,10 @@ extension WineMapViewController: CLLocationManagerDelegate {
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if locations.first != nil{
-            print("location:: (location)")
+        if let location = locations.first {
+            let span = MKCoordinateSpanMake(0.05, 0.05)
+            let region = MKCoordinateRegion(center: location.coordinate, span: span)
+            mapView.setRegion(region, animated: true)
         }
     }
     
